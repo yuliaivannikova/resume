17e47edd37e4d5fe60d04ae303bd42b3676f1951
yuliaivannikova <yulia.godyk@gmail.com>
2020-01-13 21:47:06 +0200

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,16 +6,40 @@ $('.works__slider').slick({
     slidesToScroll: 1,
     dots: false,
     autoplay: false,
+    dots: true,
     arrows: true,
     nextArrow: '<div class="works__icon works__icon--right material-icons-keyboard_arrow_right"> </div>',
     prevArrow: '<div class="works__icon works__icon--left material-icons-keyboard_arrow_left"> </div>',
+
+    responsive: [{
+            breakpoint: 1120,
+            settings: {
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+            }
+        },
+
+    ]
 
 });
 
 //skills
 
 $(function () {
-    $('#resume').on('click', function () {
+    $('.resume__on').on('click', function () {
         $('.resume').animate({
             top: "0"
         }, 1000);
@@ -61,15 +85,15 @@ $(function () {
 $(function () {
     $('.home__icon').on('click', function () {
         $('.resume').animate({
-            top: "-150%"
+            top: "-500%"
         }, 1000)
 
         $('.contact').animate({
-            top: "-150%"
+            top: "-500%"
         }, 1000)
 
         $('.works').animate({
-            top: "-150%"
+            top: "-500%"
         }, 1000)
 
         $('.skills__value--photo').animate({
@@ -110,7 +134,7 @@ $(function () {
 })
 
 $(function () {
-    $('#contact').on('click', function () {
+    $('.contact__on').on('click', function () {
         $('.contact').animate({
             top: "0"
         }, 1000);
@@ -119,10 +143,36 @@ $(function () {
 })
 
 $(function () {
-    $('#portfolio').on('click', function () {
+    $('.portfolio__on').on('click', function () {
         $('.works').animate({
             top: "0"
         }, 1000);
     })
 
 })
+
+//nav
+
+$(function () {
+    var open = $('.nav__burger'),
+        menu = $('.nav__barlist'),
+        close = $('.nav__close'),
+        overlay = $('.nav__overlay');
+    link = $('.nav__barlink')
+
+    open.on('click', function () {
+        menu.toggleClass('nav__barlist--active');
+        overlay.show();
+    });
+    close.on('click', function () {
+        menu.toggleClass('nav__barlist--active');
+        overlay.hide();
+    })
+    link.on('click', function () {
+        menu.toggleClass('nav__barlist--active');
+        overlay.hide();
+    })
+
+
+
+});
